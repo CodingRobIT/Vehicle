45f567816a91c99aa27f3cb9ea7b458449f19b37
Robin Schatzl <tmai02476@gmail.com>
2023-03-09 19:57:44 +0100

    Fertig ohne Bonus

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,13 +12,13 @@ public class Main {
         System.out.println("_______________________________________");
 
         Bicycle bicycle1 = new Bicycle(451,"Cube", 20);
-        bicycle1.accelerate(20);
+        bicycle1.accelerate(5);
 
-//        try {bicycle1.accelerate(34);
-//        }
-//        catch (NoSuchElementException myException){
-//            System.out.println("Too high");
-//        }
+        try {bicycle1.accelerate(34);
+        }
+        catch (NoSuchElementException myException){
+            System.out.println("Too high"); //Wird immer geprintet wenn unter 35 und ab 35 throwt die Exception mit nem Error
+        }
 
     }
 }
